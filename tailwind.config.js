f0d3d257ffc7897f7cf7f7d923efeb37dc49cdd2
Nurul Mukhlisa <numulaa@gmail.com>
2022-11-09 16:54:34 -0900

    connect with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{html,ejs}", "./views/partials/*.{html,ejs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
